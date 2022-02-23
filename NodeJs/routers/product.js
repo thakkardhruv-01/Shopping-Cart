3c82d31128bfadcad93162fed362ef26612098c1
Dhruv <thakkardhruv2015@gmail.com>
2022-02-23 19:15:17 +0530

    updation of delete

--- NodeJs/routers/product.js
+++ NodeJs/routers/product.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
+const multer = require('multer');
+const path = require('path');
+
 const ProductModel = require('../models/product-model');
 const CountModel = require('../models/counter-model');
 const profileModel = require('../models/profile-model');
@@ -14,24 +17,24 @@ router.get('/', (req, res) => {
 
 })
 //localhost:3000/product/admin/:profileId
-router.get('/admin/:profileId',async (req,res)=>{
-    try { 
+router.get('/admin/:profileId', async (req, res) => {
+    try {
         const profileId = req.params.profileId;
-        var productData = await ProductModel.find({profileId:profileId});
+        var productData = await ProductModel.find({ profileId: profileId });
         res.send(productData);
-        
+
     } catch (err) {
         res.send(err);
         console.log(err);
     }
 });
 //localhost:3000/product/edit/:productId
-router.get('/edit/:productId',async (req,res)=>{
-    try { 
+router.get('/edit/:productId', async (req, res) => {
+    try {
         const productId = req.params.productId;
-        var productData = await ProductModel.find({productId:productId});
+        var productData = await ProductModel.find({ productId: productId });
         res.send(productData);
-        
+
     } catch (err) {
         res.send(err);
         console.log(err);
@@ -39,15 +42,19 @@ router.get('/edit/:productId',async (req,res)=>{
 });
 //localhost:3000/product/:productId
 router.delete('/:productId', (req, res) => {
-    ProductModel.findOneAndDelete({productId:req.params.productId}, (err, doc) => {
-        if (!err) {"deleted from products: " }
+    ProductModel.findOneAndDelete({ productId: req.params.productId }, (err, doc) => {
+        if (!err) {
+            "deleted from products: "
+            CartProductModel.deleteMany({ productId: req.params.productId }, (err, doc) => {
+                if (!err) {
+                    "deleted from carts: "
+                    res.send("Item deleted");
+                }
+                else { console.log(err); }
+            })
+        }
         else { console.log(err); }
     })
-    CartProductModel.deleteMany({productId:req.params.productId}, (err, doc) => {
-        if (!err) {"deleted from carts: " }
-        else { console.log(err); }
-    })
-    res.send("Item deleted");
 })
 
 router.post('/', async (req, res) => {
@@ -56,16 +63,16 @@ router.post('/', async (req, res) => {
         var id;
         const counts = await CountModel.find({});
         console.log(counts[0].productId);
-        if(counts[0].productId==undefined){
-            id=1;
+        if (counts[0].productId == undefined) {
+            id = 1;
             oldId = counts[0].profileId;
             await CountModel.updateOne({ profileId: oldId }, [{ $set: { productId: id } }], { new: true });
 
-        }else{
-        oldId = counts[0].productId;
-        id = oldId + 1;
-        await CountModel.updateOne({ productId: oldId }, [{ $set: { productId: id } }], { new: true });
-    }
+        } else {
+            oldId = counts[0].productId;
+            id = oldId + 1;
+            await CountModel.updateOne({ productId: oldId }, [{ $set: { productId: id } }], { new: true });
+        }
 
     } catch (e) {
         console.log(e);
@@ -78,7 +85,7 @@ router.post('/', async (req, res) => {
         description: req.body.description,
         quantity: req.body.quantity,
         price: req.body.price,
-        profileId:req.body.profileId
+        profileId: req.body.profileId
     })
     product.save()
         .then(() => { res.send(product); })
@@ -104,5 +111,5 @@ router.post("/:productId", async (req, res) => {
     }
 
 })
-//hello
+
 module.exports = router;
