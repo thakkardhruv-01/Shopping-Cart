7875d2491e7cb3f12180cdc07452320a8d300745
Dhruv-Innovate <dhruv@innovatemr.com>
2022-03-09 10:57:36 +0530

    login

--- NodeJs/routers/profile.js
+++ NodeJs/routers/profile.js
@@ -6,48 +6,53 @@ const CountModel = require('../models/counter-model');
 
 router.get('/:profileId', (req, res) => {
     console.log(req.params.profileId)
-    profileModel.find({profileId:req.params.profileId},(err, doc) => {
-        if (!err) { res.send(doc);}
+    profileModel.find({ profileId: req.params.profileId }, (err, doc) => {
+        if (!err) { res.send(doc); }
         else { res.send(err); }
     })
 })
 
 router.post('/', async (req, res) => {
-    try {
-        var id;
-        const counts = await CountModel.find({});
-        if (counts.length == 0) {
-            id = 1;
+    const count = await profileModel.find({ $or: [{ phone: req.body.phone }, { email: req.body.email }] })
+    if (!count.length) {
+        try {
+            var id;
+            const counts = await CountModel.find({});
+            if (counts.length == 0) {
+                id = 1;
 
-            let newId = new CountModel({
-                profileId: id
-            });
-            newId.save()
-                .then(() => {})
-                .catch((err) => { console.log(err); });
-        }
-        else {
-            console.log("else called");
-            oldId = counts[0].profileId;
-            id = oldId + 1;
+                let newId = new CountModel({
+                    profileId: id
+                });
+                newId.save()
+                    .then(() => { })
+                    .catch((err) => { console.log(err); });
+            }
+            else {
+                console.log("else called");
+                oldId = counts[0].profileId;
+                id = oldId + 1;
 
-            await CountModel.updateOne({ profileId: oldId },[{ $set: {profileId:id} }],{ new: true });    
+                await CountModel.updateOne({ profileId: oldId }, [{ $set: { profileId: id } }], { new: true });
+            }
+        } catch (e) {
+            console.log(e);
         }
-    } catch (e) {
-        console.log(e);
+
+        let profile = new profileModel({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            role: req.body.role,
+            password: req.body.password,
+            profileId: id
+        });
+        profile.save()
+            .then(() => { res.send(profile); })
+            .catch((err) => { console.log(err); });
+    } else {
+        res.send(false);
     }
-
-    let profile = new profileModel({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        role: req.body.role,
-        password: req.body.password,
-        profileId: id
-    });
-    profile.save()
-        .then(() => { res.send(profile); })
-        .catch((err) => { console.log(err); });
 
 })
 
